a7829a8d6c457867af2fa63b775585db3ae6afef
Shivansh Tiwari <shiv1shtiwari006@gmail.com>
2026-06-17 03:42:55 +0530

    REverseLInkLIst Problem (DAy1)

---
+++ Week_3/Day_1/ReverseLinkedList.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        ListNode* curr = head;
+        while( curr!= NULL ){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev ;
+        return head;
+    }
+};
